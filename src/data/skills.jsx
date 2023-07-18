89803078a8d7689a70377982c3e699d4cb843d75
OshkarVTec <a01275287@tec.mx>
2023-07-18 13:29:11 -0600

    Update skill list

--- src/data/skills.jsx
+++ src/data/skills.jsx
@@ -1,23 +1,62 @@
-import { FaReact, FaCss3Alt, FaHtml5, FaJs, FaGit } from "react-icons/fa";
+import {
+  FaReact,
+  FaCss3Alt,
+  FaHtml5,
+  FaJs,
+  FaGitAlt,
+  FaPython,
+} from "react-icons/fa";
+import { BiMicrochip } from "react-icons/bi";
+import { TbBrandCpp, TbBrandMysql } from "react-icons/tb";
+import { BsGraphDown } from "react-icons/bs";
+import {SiElixir, SiPhp  } from "react-icons/si";
 export const SkillsData = [
   {
     name: "ReactJS",
-    icon: <FaReact size={80} />,
+    icon: <FaReact size={80} color="#61D8FB" />,
   },
   {
     name: "HTML5",
-    icon: <FaHtml5 size={80} />,
+    icon: <FaHtml5 size={80} color="#e35c26" />,
   },
   {
     name: "CSS3",
-    icon: <FaCss3Alt size={80} />,
+    icon: <FaCss3Alt size={80} color="#264de4" />,
   },
   {
     name: "JavaScript",
-    icon: <FaJs size={80} />,
+    icon: <FaJs size={80} color="#F0DB4F" />,
   },
   {
     name: "Git",
-    icon: <FaGit size={80} />,
+    icon: <FaGitAlt size={80} color="#F1502F" />,
+  },
+  {
+    name: "Embedded systems",
+    icon: <BiMicrochip size={80} />,
+  },
+  {
+    name: "Python",
+    icon: <FaPython size={80} color="#ffde57" />,
+  },
+  {
+    name: "C++",
+    icon: <TbBrandCpp size={80} />,
+  },
+  {
+    name: "Matlab",
+    icon: <BsGraphDown size={80} color="#49c3c3" />,
+  },
+  {
+    name: "MySQL",
+    icon: <TbBrandMysql size={80} color="#00758F" />,
+  },
+  {
+    name: "Elixir",
+    icon: <SiElixir size={80} color="#a222ab" />,
+  },
+  {
+    name: "PHP",
+    icon: <SiPhp size={80} color="#474a8a" />,
   },
 ];
