dd8dd430815364fed92c59f5292f54a8dba4719e
OshkarVTec <a01275287@tec.mx>
2023-07-20 14:26:58 -0600

    Change modal style

--- src/routes/ProjectDetails.jsx
+++ src/routes/ProjectDetails.jsx
@@ -1,18 +1,20 @@
 import Modal from "../components/Projects/Modal";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import { ProjectData } from "../data/projects.js";
 import classes from "./ProjectDetails.module.css";
 
 function ProjectDetails() {
   const routeId = useParams().projectId;
-  const project = ProjectData.filter((currProject)=>{return currProject.id == routeId})[0];
+  const project = ProjectData.filter((currProject) => {
+    return currProject.id == routeId;
+  })[0];
   console.log(project);
   return (
     <Modal>
       <main className={classes.details}>
-        <img src={project.image} />
         <h1 className={classes.title}>{project.title}</h1>
+        <img className={classes.photo} src={project.image} />
         <p className={classes.text}>{project.body}</p>
       </main>
     </Modal>
@@ -20,4 +22,3 @@ function ProjectDetails() {
 }
 
 export default ProjectDetails;
-
