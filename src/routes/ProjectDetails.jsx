6aa383fe9da85c0b9155c2de4d0e577a9f5ece8a
OshkarVTec <a01275287@tec.mx>
2023-07-21 10:04:03 -0600

    Add more details to project details

--- src/routes/ProjectDetails.jsx
+++ src/routes/ProjectDetails.jsx
@@ -1,6 +1,7 @@
-import Modal from "../components/Projects/Modal";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { AiOutlineCloseCircle, AiFillGithub } from "react-icons/ai";
 
+import Modal from "../components/Projects/Modal";
 import { ProjectData } from "../data/projects.js";
 import classes from "./ProjectDetails.module.css";
 
@@ -13,11 +14,32 @@ function ProjectDetails() {
   return (
     <Modal>
       <main className={classes.details}>
-        <h1 className={classes.title}>{project.title}</h1>
-        <div className={classes.imageContainer}>
+        <div className={classes.titleContainer}>
+          <h1 className={classes.title}>{project.title}</h1>
+          <div>
+            <Link to=".." className={classes.btn}>
+              <AiOutlineCloseCircle color="#333333" size="30" />
+            </Link>
+          </div>
+        </div>
+        <div className={classes.centeredContainer}>
           <img className={classes.photo} src={project.image} />u
         </div>
         <p className={classes.body}>{project.body}</p>
+        <h2>Tags</h2>
+        <ul className={classes.list}>
+          {project.tags.map((tag) => (
+            <li className={classes.tag}>{tag}</li>
+          ))}
+        </ul>
+        <div className={classes.linkContainer}>
+          <h2>Link to project</h2>
+          <div className={classes.centeredContainer}>
+            <a href={project.github} className={classes.link}>
+              <AiFillGithub size="50" />
+            </a>
+          </div>
+        </div>
       </main>
     </Modal>
   );
