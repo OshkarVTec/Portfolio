a475878a84bb6e4122305ffd0ff2a8807970ca70
OshkarVTec <a01275287@tec.mx>
2023-07-21 11:57:25 -0600

    Update header style

--- src/components/Header/MainHeader.jsx
+++ src/components/Header/MainHeader.jsx
@@ -1,26 +1,44 @@
-import { HiCode } from 'react-icons/hi';
+import { HiCode } from "react-icons/hi";
 import { Link } from "react-scroll";
 
-import classes from './MainHeader.module.css';
+import classes from "./MainHeader.module.css";
 
 function MainHeader() {
   return (
     <header className={classes.header}>
-      <h1 className={classes.logo}>
-        <HiCode size={25}/>
+      <p className={classes.logo}>
+        <HiCode size={25} />
         Portfolio - Oskar Villa
-      </h1>
+      </p>
       <nav>
-        <Link smooth={true} spy={true} to="Projects" offset={-100} className={classes.button} >
-          Projects
-        </Link>
-        <Link smooth={true} spy={true}to="Skills" offset={-100} className={classes.button} >
-          Skills
-        </Link>
-        <Link smooth={true} spy={true} to="" className={classes.button} >
+        <Link
+          smooth={true}
+          spy={true}
+          to="About"
+          offset={-100}
+          className={classes.button}
+        >
           About me
         </Link>
-        <Link smooth spy to="" className={classes.button} >
+        <Link
+          smooth={true}
+          spy={true}
+          to="Projects"
+          offset={-100}
+          className={classes.button}
+        >
+          Projects
+        </Link>
+        <Link
+          smooth={true}
+          spy={true}
+          to="Skills"
+          offset={-100}
+          className={classes.button}
+        >
+          Skills
+        </Link>
+        <Link smooth spy to="" className={classes.button}>
           Contact me
         </Link>
       </nav>
