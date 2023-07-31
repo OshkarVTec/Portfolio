b5ad986466756ad334bb761cdcb810aef818c975
OshkarVTec <a01275287@tec.mx>
2023-07-31 10:43:36 -0600

    Change path

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       {
-        path: "/",
+        path: "/Portfolio",
         element: <Content />,
         children: [
           {
