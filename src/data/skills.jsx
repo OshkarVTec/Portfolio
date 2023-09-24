d4311228342df51f4b6d5e347f76e3d4010625ce
OshkarVTec <a01275287@tec.mx>
2023-09-24 13:53:16 -0600

    add node to skills

--- src/data/skills.jsx
+++ src/data/skills.jsx
@@ -1,62 +1,67 @@
 import {
-  FaReact,
-  FaCss3Alt,
-  FaHtml5,
-  FaJs,
-  FaGitAlt,
-  FaPython,
+	FaReact,
+	FaCss3Alt,
+	FaHtml5,
+	FaJs,
+	FaGitAlt,
+	FaPython,
+	FaNodeJs,
 } from "react-icons/fa";
 import { BiMicrochip } from "react-icons/bi";
 import { TbBrandCpp, TbBrandMysql } from "react-icons/tb";
 import { BsGraphDown } from "react-icons/bs";
 import { SiElixir, SiPhp } from "react-icons/si";
 export const SkillsData = [
+	{
+		name: "ReactJS",
+		icon: <FaReact size={80} color="#61D8FB" />,
+	},
+	{
+		name: "Python",
+		icon: <FaPython size={80} color="#ffde57" />,
+	},
+	{
+		name: "C++",
+		icon: <TbBrandCpp size={80} />,
+	},
+	{
+		name: "Embedded systems",
+		icon: <BiMicrochip size={80} />,
+	},
+	{
+		name: "JavaScript",
+		icon: <FaJs size={80} color="#F0DB4F" />,
+	},
+	{
+		name: "HTML5",
+		icon: <FaHtml5 size={80} color="#e35c26" />,
+	},
+	{
+		name: "CSS3",
+		icon: <FaCss3Alt size={80} color="#264de4" />,
+	},
+	{
+		name: "Git",
+		icon: <FaGitAlt size={80} color="#F1502F" />,
+	},
+	{
+		name: "Matlab",
+		icon: <BsGraphDown size={80} color="#49c3c3" />,
+	},
+	{
+		name: "MySQL",
+		icon: <TbBrandMysql size={80} color="#00758F" />,
+	},
+	{
+		name: "Elixir",
+		icon: <SiElixir size={80} color="#a222ab" />,
+	},
+	{
+		name: "PHP",
+		icon: <SiPhp size={80} color="#474a8a" />,
+	},
   {
-    name: "ReactJS",
-    icon: <FaReact size={80} color="#61D8FB" />,
-  },
-  {
-    name: "Python",
-    icon: <FaPython size={80} color="#ffde57" />,
-  },
-  {
-    name: "C++",
-    icon: <TbBrandCpp size={80} />,
-  },
-  {
-    name: "Embedded systems",
-    icon: <BiMicrochip size={80} />,
-  },
-  {
-    name: "JavaScript",
-    icon: <FaJs size={80} color="#F0DB4F" />,
-  },
-  {
-    name: "HTML5",
-    icon: <FaHtml5 size={80} color="#e35c26" />,
-  },
-  {
-    name: "CSS3",
-    icon: <FaCss3Alt size={80} color="#264de4" />,
-  },
-  {
-    name: "Git",
-    icon: <FaGitAlt size={80} color="#F1502F" />,
-  },
-  {
-    name: "Matlab",
-    icon: <BsGraphDown size={80} color="#49c3c3" />,
-  },
-  {
-    name: "MySQL",
-    icon: <TbBrandMysql size={80} color="#00758F" />,
-  },
-  {
-    name: "Elixir",
-    icon: <SiElixir size={80} color="#a222ab" />,
-  },
-  {
-    name: "PHP",
-    icon: <SiPhp size={80} color="#474a8a" />,
-  },
+		name: "Node.js",
+		icon: <FaNodeJs size={80} color="#3c873a" />,
+	},
 ];
