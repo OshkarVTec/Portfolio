c9a1b5e915a96550eb24be055f0d83d3a51497af
OshkarVTec <a01275287@tec.mx>
2023-12-03 20:59:34 -0600

    change about appearance

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,27 +1,30 @@
 import classes from "./About.module.css";
 
 function About() {
-  return (
-    <>
-      <h1 className={classes.title}>Welcome to my portfolio!</h1>
-      <p>
-        My name is Oskar Villa, and I'm delighted to present my journey in software and web development, as
-        well as embedded systems projects. Currently pursuing a B.S. in Computer
-        Science and Technology, I've been captivated by the endless
-        possibilities that technology offers.
-      </p>
-      <p>
-        This portfolio showcases my passion for innovation and problem-solving.
-        From crafting elegant code for software applications to designing
-        dynamic websites and engineering efficient solutions for embedded
-        systems, each project reflects my commitment to continuous learning.
-      </p>
-      <p>
-        Thank you for visiting, and let's embrace the boundless potential of
-        technology together.
-      </p>
-    </>
-  );
+	return (
+		<section className={classes.container}>
+			<h1 className={classes.title}>Welcome to my portfolio!</h1>
+			<div className={classes.text}>
+				<p>
+					My name is Oskar Villa, and I'm delighted to present my journey in
+					software and web development, as well as embedded systems projects.
+					Currently pursuing a B.S. in Computer Science and Technology, I've
+					been captivated by the endless possibilities that technology offers.
+				</p>
+				<p>
+					This portfolio showcases my passion for innovation and
+					problem-solving. From crafting elegant code for software applications
+					to designing dynamic websites and engineering efficient solutions for
+					embedded systems, each project reflects my commitment to continuous
+					learning.
+				</p>
+				<p>
+					Thank you for visiting, and let's embrace the boundless potential of
+					technology together.
+				</p>
+			</div>
+		</section>
+	);
 }
 
 export default About;
