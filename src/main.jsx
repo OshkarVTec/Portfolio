e53c71576b0e606107b014e5c622e23133a78f67
OshkarVTec <a01275287@tec.mx>
2024-03-03 10:27:11 -0600

    change router to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 
 import Content from "./routes/Content";
 import RootLayout from "./routes/RootLayout";
@@ -8,28 +8,28 @@ import ProjectDetails from "./routes/ProjectDetails.jsx";
 import NotFound from "./components/NotFound/NotFound.jsx";
 import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/Portfolio",
-    element: <RootLayout />,
-    children: [
-      {
-        path: "/Portfolio",
-        element: <Content />,
-        children: [
-          {
-            path: "/Portfolio/project/:projectId",
-            element: <ProjectDetails />,
-          },
-        ],
-      },
-    ],
-  },
-  { path: "/Portfolio/*", element: <NotFound /> },
+const router = createHashRouter([
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{
+				path: "/",
+				element: <Content />,
+				children: [
+					{
+						path: "/project/:projectId",
+						element: <ProjectDetails />,
+					},
+				],
+			},
+		],
+	},
+	{ path: "/*", element: <NotFound /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
