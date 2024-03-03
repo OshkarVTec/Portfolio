2d418fee4a039be486b53a6adbe5be7efc6c6c0a
OshkarVTec <a01275287@tec.mx>
2024-03-03 08:43:17 -0600

    fix key in map

--- src/components/Contact/ContactList.jsx
+++ src/components/Contact/ContactList.jsx
@@ -1,17 +1,17 @@
-import classes from "./ContactList.module.css"
-import {SocialData} from "../../data/social.jsx"
+import classes from "./ContactList.module.css";
+import { SocialData } from "../../data/social.jsx";
 
-function Contact(){
-   return(
-      <ul className={classes.list}>
-         {SocialData.map((social) => (
-        <li key={social.name}>
-            <a href={social.link}>{social.icon}</a>
-            <h3>{social.platform}</h3>
-        </li>
-      ))}
-      </ul>
-   );
+function Contact() {
+	return (
+		<ul className={classes.list}>
+			{SocialData.map((social) => (
+				<li key={social.platform}>
+					<a href={social.link}>{social.icon}</a>
+					<h3>{social.platform}</h3>
+				</li>
+			))}
+		</ul>
+	);
 }
 
 export default Contact;
